6c528c3ed5193eac774d1de4b9bd594b4a86fc8c
rom <Chrisrom41725167>
2024-05-29 15:20:47 +0200

    inscription

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-//import Inscription from './Inscription.jsx'
+import Inscription from './Inscription.jsx'
 import Connexion from './connexion.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Connexion />
+    <Inscription />
   </React.StrictMode>,
 )
 
