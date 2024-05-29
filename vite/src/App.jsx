5e460cb97a7401e2dca36dcb354bb3c1af1ac19c
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-05-29 16:50:19 +0200

    29/05/2024 - 16:50

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 import Accueil from './pages/Accueil';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/Accueil" element={<Accueil />} />
       </Routes>
