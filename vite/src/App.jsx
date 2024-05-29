8b89f4fbe73d7e8e35d026b57afb6323e64e91a4
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-05-29 15:37:52 +0200

    29/05/2024 : 29/05/2024

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HallOfFame from './pages/HallOfFame';
-import Profile from './pages/Profile';
-import Navbar from './components/Navbar/Navbar';
-import Accueil from './pages/Accueil'
-import Groupe from './pages/Groupe';
+import Accueil from './pages/Accueil';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/Accueil" element={<Accueil />} />
-        <Route path="/Groupe" element={<Groupe />} />
-        <Route path="/Halloffame" element={<HallOfFame />} />
-        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
