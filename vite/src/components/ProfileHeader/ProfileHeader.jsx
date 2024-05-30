81b7d72d84d7f0e7e772d42a65bb0b0a4dc70c97
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-05-30 16:13:03 +0200

    30/05/2024 - 16:12

--- vite/src/components/ProfileHeader/ProfileHeader.jsx
+++ vite/src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import './ProfileHeader.css';
 
+const user = [
+        { 
+                Nom: ' Thorkell', 
+                Prenom: 'Le Grand', 
+                Description:'Ouvert communicatif aime rencontrer de nouvelles personnes et élargir son réseau social. Trouve de lénergie dans les interactions avec les autres et aime être au centre de lactivité sociale.',
+        },
+    ];
+
 const ProfileHeader = () => {
   return (
     <div className="ProfileHeader">
       <div className="profile-info">
         <div className="profile-picture"><img src="profil.jpg" alt="profile-picture" /></div>
-        <h3>JACQUES WEBSTER</h3>
-      </div>
-      <div className="description">
-        <h4>Description</h4>
-        <p>Ouvert, communicatif, aime rencontrer de nouvelles personnes et élargir son réseau social. Trouve de l'énergie dans les interactions avec les autres et aime être au centre de l'activité sociale.</p>
-      </div>
+        {user.map((item) => (
+            <h3>
+                {item.Nom} {item.Prenom}
+            </h3>
+        ))}
+        {user.map((item) => (
+            <p>
+                {item.Description}
+            </p>
+        ))}
+        </div>
     </div>
   );
 }
