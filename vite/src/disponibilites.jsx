cdc959cecec76ab2c4096e20f951677e6bc25290
Roméo Vandoolaeghe <romeo.vandoolaeghe@student.junia.com>
2024-06-05 11:59:55 +0200

    feat: Add fetching and displaying of disponibilites in Disponibilites component

--- vite/src/disponibilites.jsx
+++ vite/src/disponibilites.jsx
@@ -14,6 +14,26 @@ function Disponibilites() {
 
 
 
+    const [disponibilites, setDisponibilites] = useState([]);
+
+    // Utiliser useEffect pour récupérer les disponibilités au montage du composant
+    useEffect(() => {
+        const fetchDisponibilites = async () => {
+
+            try {
+                const response = await axios.get('http://localhost:3000/disponibilites');
+                if (response.data && Array.isArray(response.data)) {
+                    setDisponibilites(response.data);
+                } else {
+                    console.error('Les données reçues ne sont pas valides', response.data);
+                }
+            } catch (error) {
+                console.error('Erreur lors de la récupération des disponibilités:', error);
+            }
+        };
+        fetchDisponibilites();
+    }, []);
+
 
     // Fonction pour gérer les changements dans le formulaire
     const handleChange = (e) => {
@@ -50,6 +70,7 @@ function Disponibilites() {
         axios.post(url, formData)
             .then(response => {
                 console.log('Réponse du serveur:', "Disponibilité enregistrée avec succès");
+                setDisponibilites([...disponibilites, formData]); // Mettre à jour la liste des disponibilités
             })
             .catch(error => {
                 console.error('Erreur lors de la requête POST:', error);
@@ -89,8 +110,14 @@ function Disponibilites() {
                 </div>
                 <button type="submit">Ajouter ma disponibilite</button>
             </form>
-
             <h2>Liste des disponibilités</h2>
+            <ul>
+                {disponibilites.map((dispo, index) => (
+                    <li key={index}>
+                        Début: {new Date(dispo.Date_Dispo_debut).toLocaleString()} - Fin: {new Date(dispo.Date_Dispo_fin).toLocaleString()}
+                    </li>
+                ))}
+            </ul>
 
 
         </div>
--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -15,9 +15,9 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <Inscription/> */}
+    <Inscription/>
     <Connexion/>
-    {/* <Disponibilites/> */}
+    <Disponibilites/>
   </React.StrictMode>,
 )
 
