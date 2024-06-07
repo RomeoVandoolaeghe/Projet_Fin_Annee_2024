0c8e1d1326279ee50fd5d5ca49752d68418eadf5
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-07 12:17:46 +0200

    ok

--- vite/src/components/GroupProfile/GroupProfile.jsx
+++ vite/src/components/GroupProfile/GroupProfile.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './GroupProfile.css';
 
-const groups = [
-  { name: 'Ragnar Lodbrok'},
-  
-];
+const GroupProfile = () => {
+  const [groups, setGroups] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:3001/groupes')
+      .then(response => {
+        setGroups(response.data);
+      })
+      .catch(error => {
+        console.error('Erreur lors de la récupération des groupes:', error);
+      });
+  }, []);
 
-const GroupProfile = ({ isEditMode }) => {
   return (
     <div className="groups-list">
       <h4>Mes groupes</h4>
       <ul>
         {groups.map((group, index) => (
           <li key={index}>
-            {group.name}
+            {group.Nom_groupe}
           </li>
         ))}
       </ul>
