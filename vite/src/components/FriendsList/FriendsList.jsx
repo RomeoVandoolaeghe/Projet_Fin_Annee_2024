529fd3b964f2258b861fd347dd41f27234d31b84
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-10 10:16:29 +0200

    Ajustement FriendList

--- vite/src/components/FriendsList/FriendsList.jsx
+++ vite/src/components/FriendsList/FriendsList.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './FriendsList.css';
 import { Link } from 'react-router-dom';
-
 
 const friends = [
   { name: 'Ragnar Lodbrok', status: 'online' },
@@ -14,18 +13,37 @@ const friends = [
   { name: 'Kjetill Eriksson', status: 'online' },
 ];
 
+const group = [
+  { name: 'SEIRIN' },
+  { name: 'JABBERWORKS' },
+  { name: 'TOHO' },
+];
+
 const FriendsList = () => {
+  const [groups, setGroups] = useState(group);
+
   return (
-    <div className="friends-list">
-      <h4>Liste d’amis</h4>
-      <ul>
-        {friends.map((friend, index) => (
-          <li key={index} className={friend.status}>
-            {friend.name}
-            <span className={`status ${friend.status === 'online' ? 'green' : 'red'}`}></span>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className="list-container friends-list">
+        <h4>Liste d’amis</h4>
+        <ul>
+          {friends.map((friend, index) => (
+            <li key={index} className={friend.status}>
+              {friend.name}
+              <span className={`status ${friend.status === 'online' ? 'green' : 'red'}`}></span>
+            </li>
+          ))}
+        </ul>
+        <button><Link to="/AddFriend">Ajouter un amis</Link></button>
+        <h4>Mes groupes</h4>
+        <ul>
+          {groups.map((group, index) => (
+            <li key={index}>
+              {group.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
