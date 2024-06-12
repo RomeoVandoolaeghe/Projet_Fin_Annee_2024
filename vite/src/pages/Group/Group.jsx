29b3bde5b4bbbddb8cde6ae4089d51e6b238226a
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-12 16:05:06 +0200

    update Groupe

--- vite/src/pages/Group/Group.jsx
+++ vite/src/pages/Group/Group.jsx
@@ -1,45 +1,22 @@
-import React,{ useEffect} from 'react';
-import ScrollReveal from 'scrollreveal';
+import React from 'react';
 import './Group.css';
 import { Link } from 'react-router-dom';
 import GroupCard from '../../components/GroupCard/GroupCard';
 import { FaBars, FaUser, FaHome, FaCalendarAlt, FaUsers, FaTrophy, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import '../../pages/Chat/Chat';
 
 const groups = [
   { name: 'ASTRO WORD', image: ' A ' },
-  { name: 'JUNIA ISEN', image: 'I' },
-  { name: 'IA', image: 'IA' },
-  { name: 'Solo', image: 'S' },
-  { name: 'Skyclue', image: 'S' },
-  { name: 'Fairy', image: 'F' },
-  { name: 'LAFAME', image: 'L' },
-  { name: 'Sabertooth', image: 'S' },
-  { name: 'Mermaid', image: 'M' },
 ];
 
 
 const Group = () => {
-
-  useEffect(() => {
-    // Configuration de base de ScrollReveal
-    const sr = ScrollReveal({
-      origin: 'bottom',
-      distance: '20px',
-      duration: 500,
-      delay: 100,
-      reset: true, // Animation réapparaît à chaque défilement
-    });
-
-    // Appliquer l'animation aux éléments avec la classe "reveal"
-    sr.reveal('.reveal');
-  }, []);
-
   return (
     <>
-      <div className='header reveal'>
-        <h2>Groupe</h2>
+      <div className='header'>
+        <h2>Groupe <FaUsers /></h2>
       </div>
-      <div className="group-page reveal">
+      <div className="group-page">
           <div className="header-actions">
             <input type="text" placeholder="Rechercher" />
           </div>
@@ -49,9 +26,11 @@ const Group = () => {
               <p><strong>Nouveau groupe</strong> <br /><FaUsers /></p>
             </Link>
           </div>
-          {groups.map((group, index) => (
-            <GroupCard key={index} name={group.name} image={group.image} />
-          ))}
+          
+            {groups.map((group, index) => (
+              <GroupCard key={index} name={group.name} image={group.image} />
+            ))}
+          
         </div>
       </div>
     </>
