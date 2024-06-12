b2fcc937af68de7c8e07925c8cfc637c23330b17
Lauric1 <lauricgokar@gmail.com>
2024-06-12 14:17:19 +0200

    new last

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -6,14 +6,14 @@ import CreateOutput from './pages/CreateOutput/CreateOutput.jsx';
 import Calendar from './pages/Calendars/Calendars.jsx';
 import Login from './pages/Login/Login.jsx';
 import Chat from './pages/Chat/Chat.jsx';
-import Home from './pages/Accueil/Accueil';
-import ProfilePage from './pages/Profile/Profile';
-import Parametres from './pages/Parametres/Parametres';
-import HallofFame from './pages/HallofFame/HallofFame';
-import AddFriend from './pages/AddFriend/AddFriend';
-import Group from './pages/Group/Group';
-import CreateGroup from './pages/CreateGroup/CreateGroup';
-import SideBar from './components/SideBar/SideBar';
+import Home from './pages/Accueil/Accueil.jsx';
+import ProfilePage from './pages/Profile/Profile.jsx';
+import Parametres from './pages/Parametres/Parametres.jsx';
+import HallofFame from './pages/HallofFame/HallofFame.jsx';
+import AddFriend from './pages/AddFriend/AddFriend.jsx';
+import Group from './pages/Group/Group.jsx';
+import CreateGroup from './pages/CreateGroup/CreateGroup.jsx';
+import SideBar from './components/SideBar/SideBar.jsx';
 import "./App.css"
 
 function App() {
