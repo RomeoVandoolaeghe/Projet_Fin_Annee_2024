55a18751e5b6fa0c383facf2e28dfc2c22fa591d
Lauric1 <lauricgokar@gmail.com>
2024-06-12 14:38:22 +0200

    start

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
+// Importation des composants nécessaires depuis react-router-dom
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Importation des composants dans le dossier "components"
 import Navbar from './components/Navbar/Navbar.jsx';
 import Events from './pages/Events/Events.jsx';
 import CreateOutput from './pages/CreateOutput/CreateOutput.jsx';
@@ -14,15 +18,18 @@ import AddFriend from './pages/AddFriend/AddFriend.jsx';
 import Group from './pages/Group/Group.jsx';
 import CreateGroup from './pages/CreateGroup/CreateGroup.jsx';
 import SideBar from './components/SideBar/SideBar.jsx';
+// Importation des styles globaux de l'application
 import "./App.css"
 
 function App() {
   return (
     <Router>
+      {/* Afficher du logo,de la barre de navigation et de la barre latérale seulement sur toute les pages sauf la page de login */}
       {window.location.pathname !== '/' && <div className="logo"><img src="logo.png" alt="Logo du site" /></div>}
       {window.location.pathname !== '/' && <Navbar />}
       {window.location.pathname !== '/' && <SideBar />}
       <Routes>
+        {/* Définition des différentes routes */}
         <Route path='/' element={<Login />} />
         <Route path='/CreateOutput' element={<CreateOutput />} />
         <Route path="/Events" element={<Events />} />
