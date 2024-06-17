585892ab4560fb01a1bbdb03d63cb45fe38cacc7
Lauric1 <lauricgokar@gmail.com>
2024-06-17 13:38:31 +0200

    suite

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,3 +1,5 @@
+// Importation des composants dans le dossier "components"
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
@@ -45,51 +47,3 @@ function App() {
 }
 
 export default App;
-/*
-// Importation des composants dans le dossier "components"
-import Navbar from './components/Navbar/Navbar.jsx';
-import Events from '../pages/user/Events/Events.jsx';
-import CreateOutput from '../pages/user/CreateOutput/CreateOutput.jsx';
-import Calendar from '../pages/user/Calendars/Calendars.jsx';
-import Login from '../pages/user/Login/Login.jsx';
-import Chat from '../pages/user/CHAT/CHAT.jsx';
-import Home from '../pages/user/Accueil/Accueil.jsx';
-import ProfilePage from '../pages/user/Profile/Profile.jsx';
-import Parametres from '../pages/user/Parametres/Parametres.jsx';
-import HallofFame from '../pages/user/HallofFame/HallofFame.jsx';
-import AddFriend from '../pages/user/AddFriend/AddFriend.jsx';
-import Group from '../pages/user/Group/Group.jsx';
-import CreateGroup from '../pages/user/CreateGroup/CreateGroup.jsx';
-import SideBar from './components/Sidebar/Sidebar.jsx';
-// Importation des styles globaux de l'application
-import "./App.css"
-
-function App() {
-  return (
-    <Router>
-      
-      {window.location.pathname !== '/' && <div className="logo"><img src="logo.png" alt="Logo du site" /></div>}
-      {window.location.pathname !== '/' && <Navbar />}
-      {window.location.pathname !== '/' && <SideBar />}
-      <Routes>
-
-        <Route path='/' element={<Login />} />
-        <Route path='/CreateOutput' element={<CreateOutput />} />
-        <Route path="/Events" element={<Events />} />
-        <Route path='/Calendar' element={<Calendar />} />
-        <Route path="/CHAT" element={<Chat />} />
-        <Route path="/Accueil" element={<Home />} />
-        <Route path="/Profile" element={<ProfilePage />} />
-        <Route path="/Group" element={<Group />} />
-        <Route path="/Parametres" element={<Parametres />} />
-        <Route path="/HallofFame" element={<HallofFame />} />
-        <Route path="/AddFriend" element={<AddFriend />} />
-        <Route path="/CreateGroup" element={<CreateGroup />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-
-*/
