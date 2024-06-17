12562369099466eb78be829ac805accf4212d0ab
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-17 12:50:57 +0200

    OK

--- vite/src/pages/admin/accueil-admin/accueil-admin.jsx
+++ vite/src/pages/admin/accueil-admin/accueil-admin.jsx
@@ -1,49 +1,16 @@
 import React from 'react';
-import UpComingEvents from '../../../components/UpComingEvents/UpComingEvents';
-import axios from 'axios'
+import './accueil-admin.css';
 
-
-
-function Home ({ name }) {
-
-  axios.post("http://localhost:3000/acces", { withCredentials: true })
-      .then(response => {
-        console.log('Réponse de la requête POST:', response);
-        // if (response.status === 201) {
-        //   window.location.href = 'http://localhost:5173';
-        // }
-      })
-      .catch(error => {
-        console.error('Erreur lors de la requête POST:', error);
-});
-
-  const events = [
-    {
-      title: "Palais des Beaux - Arts",
-      location: "Lille",
-      time: "13:30 - 17:30",
-    },
-    {
-      title: "Palais des Beaux - Arts",
-      location: "Lille",
-      time: "13:30 - 17:30",
-    },
-  ];
-  
-  name = 'Hugo';
+function accueil_admin () {
     
   return ( 
     <>
-    <div className='header'>
-      <h2>Hello {name} !!!</h2>
-    </div>
-      <div className="home">
-        <UpComingEvents events={events} />
+      <div className='header'>
+        <h2>Hello Yecir !!</h2>
       </div>
     </>
-
   );
 }
 
+export default accueil_admin;
 
-export default Home;
