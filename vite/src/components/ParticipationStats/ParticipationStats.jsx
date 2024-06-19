fdecc7d0a592e771044d415027c1f150e3bd88bc
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-19 14:42:17 +0200

    update

--- vite/src/components/ParticipationStats/ParticipationStats.jsx
+++ vite/src/components/ParticipationStats/ParticipationStats.jsx
@@ -4,9 +4,20 @@ import './ParticipationStats.css';
 const ParticipationStats = ({ totalEvents, organizedEvents, participationRate }) => (
   <div className="stats-section">
     <h4>Statistiques de Participation</h4>
-    <p>Total de sorties : {totalEvents}</p>
-    <p>Événements organisés : {organizedEvents}</p>
-    <p>Taux de participation : {participationRate}%</p>
+    <div className="stats-grid">
+      <div className="stat-item">
+        <p>Total de sorties :</p>
+        <p>{totalEvents}</p>
+      </div>
+      <div className="stat-item">
+        <p>Événements organisés :</p>
+        <p>{organizedEvents}</p>
+      </div>
+      <div className="stat-item">
+        <p>Taux de participation :</p>
+        <p>{participationRate}%</p>
+      </div>
+    </div>
   </div>
 );
 
