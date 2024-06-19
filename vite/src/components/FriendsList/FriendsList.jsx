0490cec8fdab26c64744ce76f2ee4f2ae8171d90
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-19 15:50:06 +0200

    UPDATE

--- vite/src/components/FriendsList/FriendsList.jsx
+++ vite/src/components/FriendsList/FriendsList.jsx
@@ -49,7 +49,7 @@ const FriendsList = () => {
       <div className="list-container friends-list">
         <div className="friends-list-header">
           <h4>Liste d’amis</h4>
-            <Link to="/AddFriend" className='button_2'><span className="toggle-del" >Gérer des amis</span></Link>
+            <Link to="/AddFriend"><button className="toggle-del" >Gérer des amis</button></Link>
         </div>
         <ul>
           {friends.map((friend, index) => (
