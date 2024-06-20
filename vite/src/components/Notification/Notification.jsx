207aeeb3474a7387b5f398d627c89408d41af72e
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-20 04:26:19 +0200

    Update Notification.jsx

--- vite/src/components/Notification/Notification.jsx
+++ vite/src/components/Notification/Notification.jsx
@@ -5,7 +5,7 @@ function NotificationCard({ notification }) {
   return (
     <div className="notification-card">
       <div className="notification-details">
-        <span>{`${notification.count} nouveau message${notification.count > 1 ? 's' : ''} dans ${notification.group}`}</span>
+        <span> {`${notification.count}  nouveau message ${notification.count > 1 ? 's' : ''} dans ${notification.group}`}</span>
       </div>
     </div>
   );
