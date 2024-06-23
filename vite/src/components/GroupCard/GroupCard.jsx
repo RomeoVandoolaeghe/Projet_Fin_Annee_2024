61894ed986738b881432fb008be664de333dff08
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-23 23:01:49 +0200

    Update GroupCard.jsx

--- vite/src/components/GroupCard/GroupCard.jsx
+++ vite/src/components/GroupCard/GroupCard.jsx
@@ -7,34 +7,26 @@ import DetailGroup from '../../components/DetailGroup/DetailGroup';
 import { Link } from 'react-router-dom';
 import './GroupCard.css';
 
-// Définition du composant GroupCard
 const GroupCard = ({ id, name, image, color, onDelete, onClick }) => {
-  // État pour gérer la visibilité du menu déroulant
   const [dropdownVisible, setDropdownVisible] = useState(false);
-  // État pour gérer la visibilité de la fenêtre modale
   const [showModal, setShowModal] = useState(false);
-  // Référence à l'élément du menu déroulant
   const dropdownRef = useRef(null);
 
-  // Fonction pour basculer la visibilité du menu déroulant
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   };
 
-  // Fonction pour afficher la fenêtre modale et masquer le menu déroulant
   const handleShowModal = () => {
     setShowModal(true);
     setDropdownVisible(false);
   };
 
-  // Fonction pour gérer les clics à l'extérieur du menu déroulant et le masquer
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setDropdownVisible(false);
     }
   };
 
-  // Utilisation de useEffect pour ajouter un écouteur d'événements lors du montage du composant et le supprimer lors du démontage
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
@@ -42,14 +34,15 @@ const GroupCard = ({ id, name, image, color, onDelete, onClick }) => {
     };
   }, []);
 
-  // Fonction pour gérer la suppression d'un groupe
   const handleDelete = (e) => {
     e.preventDefault();
     onDelete(id);
   };
 
+  
+
   return (
-    <div className="group-card" onClick={() => onClick(id, name)}>
+    <div className="group-card" onClick={() => onClick(id,name)}>
       <FontAwesomeIcon
         icon={faEllipsisV}
         className="group-options"
