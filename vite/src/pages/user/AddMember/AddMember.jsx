4f9b90073faa827de4c562751e8cf8ddea02da2d
BYecir02 <mohamed-yecir.badirou@student.junia.com>
2024-06-24 12:17:44 +0200

    maj

--- vite/src/pages/user/AddMember/AddMember.jsx
+++ vite/src/pages/user/AddMember/AddMember.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './AddMember.css';
 import Navbar from '../../../components/Navbar/Navbar';
+import { useNavigate } from 'react-router-dom';
 
-function AddMember() {
+function AddMember({ onMemberAdded }) {
     const [memberName, setMemberName] = useState('');
     const [friends, setFriends] = useState([]);
     const groupID = localStorage.getItem('idgroupe');
+    const navigate = useNavigate();
 
     useEffect(() => {
         // Récupérer la liste des amis de l'utilisateur
@@ -21,11 +23,13 @@ function AddMember() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Logique pour ajouter le membre au chat
+        // Logique pour ajouter le membre au groupe
         axios.post('http://localhost:3000/add_member', { username: memberName, ID_Groupe: groupID }, { withCredentials: true })
             .then(response => {
                 console.log('Membre ajouté :', response.data);
                 setMemberName('');
+                onMemberAdded({ username: memberName }); // Envoyer les informations du membre ajouté
+                navigate('/Chat'); // Rediriger vers la page de chat après l'ajout
             })
             .catch(error => {
                 console.error('Erreur lors de l\'ajout du membre :', error);
@@ -48,7 +52,7 @@ function AddMember() {
                         >
                             <option value="">Sélectionnez un membre</option>
                             {friends.map(friend => (
-                                <option key={friend.ID_Utilisateur} value={friend.Pseudo}>
+                                <option key={friend.ID_utilisateur} value={friend.Pseudo}>
                                     {friend.Pseudo}
                                 </option>
                             ))}
