566f3be6789eb7339b7a94861a5cbaf15e1f6de8
Hugo HELLEBOID <hugo.helleboid@student.junia.com>
2024-06-26 14:43:47 +0200

    Revert "Merge branch 'frontend' of https://github.com/RomeoVandoolaeghe/Projet_Fin_Annee_2024 into frontend"
    This reverts commit df03e772ef3e91c4900368a6f1062df802a52f9c, reversing
    changes made to 6e010b8834ff7fe0722f8f8b92a3940fbd624645.

--- vite/src/components/LinePlan/LinePlan.jsx
+++ vite/src/components/LinePlan/LinePlan.jsx
@@ -1,135 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import ScrollReveal from 'scrollreveal';
 import './LinePlan.css';
 
-const LinePlan = () => {
-  const [datas, setDatas] = useState([]); // État pour stocker les données des sorties
-  const [searchTerm, setSearchTerm] = useState(''); // État pour le terme de recherche
-  const [filterLocation, setFilterLocation] = useState(''); // État pour le filtre de lieu
-  const [sortType, setSortType] = useState(''); // État pour le type de tri
-  const [selectedEvent, setSelectedEvent] = useState(null); // État pour l'événement sélectionné
-  const [locations, setLocations] = useState([]); // État pour stocker les lieux uniques
 
+const LinePlan = () => {
+  const [data_sorties, setData_sorties] = useState([]); // État pour stocker les données des sorties
+  const [data_invitations, setData_invit] = useState([]); // État pour stocker les données des invitations
   // useEffect pour récupérer les données des sorties et initialiser ScrollReveal
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchSorties = async () => {
       try {
         const response = await axios.get('http://localhost:3000/sorties', { withCredentials: true });
-        setDatas(response.data);
-        
-        // Récupérer les lieux uniques pour les options de filtre
-        const uniqueLocations = [...new Set(response.data.map(data => data.Lieu))];
-        setLocations(uniqueLocations);
+        setData_sorties(response.data);
+
+        console.log("sorties", response.data);
+
       } catch (error) {
         console.error('Erreur lors de la récupération des sorties:', error);
       }
     };
 
-    fetchData();
+    fetchSorties();
 
-    const sr = ScrollReveal({
-      origin: 'bottom',
-      distance: '20px',
-      duration: 500,
-      delay: 100,
-      reset: true,
-    });
-    sr.reveal('.reveal');
+    const fetchInvitations = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/invitations', { withCredentials: true });
+        setData_invit(response.data);
+        console.log("invitations", response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des invitations:', error);
+      }
+    };
+
+    fetchInvitations();
+
   }, []);
 
-  // Fonction pour gérer les changements du champ de recherche
-  const handleSearchChange = (e) => {
-    setSearchTerm(e.target.value);
+
+  const handleAccept = async (id) => {
+    try {
+      await axios.post('http://localhost:3000/accepter_invitation', { ID_Sortie: data_invitations[0].ID_Sortie }, { withCredentials: true });
+      console.log('Invitation acceptée');
+      alert('Invitation acceptée');
+      location.reload();
+    } catch (error) {
+      console.error('Erreur lors de l\'acceptation de l\'invitation:', error);
+    }
   };
 
-  // Fonction pour gérer les changements du filtre de lieu
-  const handleFilterChange = (e) => {
-    setFilterLocation(e.target.value);
-  };
 
-  // Fonction pour gérer les changements du type de tri
-  const handleSortChange = (e) => {
-    setSortType(e.target.value);
-  };
-
-  // Fonction pour gérer la sélection d'un événement
-  const handleEventClick = (event) => {
-    setSelectedEvent(event);
-  };
-
-  // Fonction pour fermer la modal de l'événement sélectionné
-  const closeModal = () => {
-    setSelectedEvent(null);
-  };
-
-  // Filtrer et trier les données selon les critères de recherche, filtre et tri
-  const filteredData = datas
-    .filter(data => 
-      data.Description_Sortie.toLowerCase().includes(searchTerm.toLowerCase()) &&
-      (filterLocation ? data.Lieu === filterLocation : true)
-    )
-    .sort((a, b) => {
-      if (sortType === 'date') {
-        return new Date(a.Date_Sortie) - new Date(b.Date_Sortie);
-      } else if (sortType === 'titre') {
-        return a.Description_Sortie.localeCompare(b.Description_Sortie);
-      } else if (sortType === 'heure') {
-        return new Date(a.Date_Sortie).getTime() - new Date(b.Date_Sortie).getTime(); // Trier par heure
-      } else if (sortType === 'lieu') {
-        return a.Lieu.localeCompare(b.Lieu);
-      }
-      return 0;
-    });
+console.log("les invitations: ",data_invitations)
 
   return (
     <div>
-      <div className="filters">
-        <input 
-          type="text" 
-          placeholder="Rechercher une activité..." 
-          value={searchTerm} 
-          onChange={handleSearchChange} 
-        />
-        <select value={filterLocation} onChange={handleFilterChange}>
-          <option value="">Tous les lieux</option>
-          {locations.map((location, index) => (
-            <option key={index} value={location}>{location}</option>
-          ))}
-        </select>
-        <select value={sortType} onChange={handleSortChange}>
-          <option value="">Trier par</option>
-          <option value="date">Date</option>
-          <option value="titre">Titre</option>
-          <option value="heure">Heure</option>
-          <option value="lieu">Lieu</option>
-        </select>
-      </div>
+
       <div className="reveal">
         <div className="val">
           <span>Date</span>
           <span>Heure</span>
           <span>Titre</span>
-          <span>Participant</span>
+          <span>Participants</span>
+          <span>Créateur</span>
           <span>Lieu</span>
         </div>
-        {filteredData.map((data, index) => (
-          <div key={index} className="item">
+        {data_sorties.map((data, index) => (
+          <div key={index} className="val" id="events">
             <span>{new Date(data.Date_Sortie).toLocaleDateString()}</span>
             <span>{new Date(data.Date_Sortie).toLocaleTimeString()}</span>
-            <span>{data.Description_Sortie}</span>
-            <span>{data.Nb_personnes}</span>
+            <span>{data.Titre_Sortie}</span>
+            <span>{data.nb_personnes}</span>
+            <span>{data.ID_Creator}</span>
             <span>{data.Lieu}</span>
           </div>
         ))}
       </div>
-      {selectedEvent && (
-        <EventDetails 
-          event={selectedEvent} 
-          isOpen={!!selectedEvent} 
-          onRequestClose={closeModal} 
-        />
-      )}
+
+
+
+      <div className='header'>
+        <h2>Invitations</h2>
+      </div>
+
+      <div className="reveal">
+        <div className="val">
+          <span>Date</span>
+          <span>Heure</span>
+          <span>Titre</span>
+          <span>Participants</span>
+          <span>Créateur</span>
+          <span>Lieu</span>
+          <span>Invitation</span>
+        </div>
+        {data_invitations.map((data, index) => (
+          <div key={index} className="val" id="events">
+            <span>{new Date(data.Date_Sortie).toLocaleDateString()}</span>
+            <span>{new Date(data.Date_Sortie).toLocaleTimeString()}</span>
+            <span>{data.Titre_Sortie}</span>
+            <span>{data.nb_personnes}</span>
+            <span>{data.ID_Creator}</span>
+            <span>{data.Lieu}</span>
+            <span>
+              <button title="Accepter" onClick={() => handleAccept(data.ID_Sortie)}>YES</button>
+              <button title="Refuser" onClick={() => handleRefuse(data.ID_Sortie)}>NO</button>
+            </span>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   );
 }
